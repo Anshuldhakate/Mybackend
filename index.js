4bf1fca5d490e46cc6b54f3fe403291552b57955
unknown <anshuldhakate11@gmail.com>
2024-01-30 11:11:08 +0530

    done

---
+++ index.js
@@ -0,0 +1,32 @@
+const express= require("express")
+const connection= require("./db")
+const {userRouter}= require("./Routes/user.routes")
+const cors = require('cors');
+
+
+// const {noteRouter}= require("./routes/note.routes")
+
+const app= express();
+app.use(cors());
+
+app.use(express.json())
+app.use("/users", userRouter)
+// app.use("/notes", noteRouter)
+
+app.get("/", (req, res)=>{
+    res.send("Welcome Fitness")
+})
+
+
+
+app.listen(8080,async()=>{
+   try{
+    await connection
+    console.log("connected to DB")
+    console.log("server is running on port 3000")
+   }
+   catch(err){
+    console.log(err)
+   }
+    
+})
